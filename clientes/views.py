4dd5fc77059abd7101770a2760bd8435079ae12c
soulevil09 <igorgalloseabra@gmail.com>
2022-10-31 13:50:56 -0300

    Parte 3

--- clientes/views.py
+++ clientes/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.http import HttpResponse, JsonResponse
 from .models import Cliente, Carro
 import re
 from django.core import serializers
 import json
+from django.views.decorators.csrf import csrf_exempt
+
 
 def clientes(request):
     if request.method == 'GET':    
@@ -40,6 +43,39 @@ def clientes(request):
 
 def att_cliente(request):
     id_cliente = request.POST.get('id_cliente')
+
     cliente = Cliente.objects.filter(id=id_cliente)
-    cliente_json = json.loads(serializers.serialize('json', cliente))[0]['fields']
-    return JsonResponse(cliente_json)
+    carros = Carro.objects.filter(cliente=cliente[0])
+    
+    clientes_json = json.loads(serializers.serialize('json', cliente))[0]['fields']
+    carros_json = json.loads(serializers.serialize('json', carros))
+    carros_json = [{'fields':carro['fields'], 'id': carro['pk']} for carro in carros_json]
+    data = {'cliente': clientes_json, 'carros': carros_json}
+    return JsonResponse(data)
+
+@csrf_exempt
+def update_carro(request, id):
+    nome_carro = request.POST.get('carro')
+    placa = request.POST.get('placa')
+    ano = request.POST.get('ano')
+
+    carro = Carro.objects.get(id=id)
+    list_carros = Carro.objects.exclude(id=id).filter(placa=placa)
+
+    if list_carros.exists():
+        return HttpResponse('Placa existente')
+
+    carro.carro = nome_carro
+    carro.placa = placa
+    carro.ano = ano
+    carro.save()
+    return HttpResponse('Dados alterados com sucesso')
+
+def excluir_carro(request, id):
+    try:
+        carro = Carro.objects.get(id=id)
+        carro.delete()
+        return redirect(reverse('clientes')+f'?aba=att_cliente&id_cliente={id}')
+    except:
+        return redirect(reverse('clientes')+f'?aba=att_cliente&id_cliente={id}')
+
